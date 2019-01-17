c42955f4dd301a2511f7d3a5300cc6cffe5dadc2
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-01-17 13:47:44 +0100

    Lagt till Name property för Samurai

--- EfSamurai.Data/EfSamurai.Data/Migrations/20190117124645_AddingNamePropertyToSamurai.Designer.cs
+++ EfSamurai.Data/EfSamurai.Data/Migrations/20190117124645_AddingNamePropertyToSamurai.Designer.cs
@@ -3,14 +3,16 @@ using EfSamurai.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EfSamurai.Data.Migrations
 {
     [DbContext(typeof(SamuraiContext))]
-    partial class SamuraiContextModelSnapshot : ModelSnapshot
+    [Migration("20190117124645_AddingNamePropertyToSamurai")]
+    partial class AddingNamePropertyToSamurai
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- EfSamurai.Data/EfSamurai.Domain/Class1.cs
+++ EfSamurai.Data/EfSamurai.Domain/Class1.cs
@@ -5,5 +5,6 @@ namespace EfSamurai.Domain
     public class Samurai
     {
         public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
