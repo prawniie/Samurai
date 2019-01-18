85659c38eb470c1c1c0db726f7de982fab851279
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-01-18 12:06:45 +0100

    Kan lägga till battles med lite egenskaper

--- EfSamurai.Data/EfSamurai.App/Program.cs
+++ EfSamurai.Data/EfSamurai.App/Program.cs
@@ -1,6 +1,7 @@
 ﻿using EfSamurai.Data;
 using EfSamurai.Domain;
 using System;
+using System.Collections.Generic;
 
 namespace EfSamurai.App
 {
@@ -10,10 +11,51 @@ namespace EfSamurai.App
         {
             //ClearDatabase();
             AddOneSamurai();
+            AddSomeSamurais();
+            AddSomeBattles();
+        }
+
+        private static void AddSomeBattles()
+        {
+            var context = new SamuraiContext();
+
+            var battleEvents = new List<BattleEvent>
+            {
+                new BattleEvent { Description = "The leader Goron initiated the first attack", TimeOfEvent = new DateTime(900,1,20) }
+            };
+
+            var battleLog = new BattleLog
+            {
+                Name = "Goron War log",
+                BattleEvents = battleEvents
+            };
+
+            var battle1 = new Battle
+            {
+                Name = "The Goron War",
+                BattleLog = battleLog
+            };
+
+            context.Battles.Add(battle1);
+            context.SaveChanges();
+
+        }
+
+        private static void AddSomeSamurais()
+        {
+            var context = new SamuraiContext();
+
+            var samurai1 = new Samurai { Name = "Link" };
+            var samurai2 = new Samurai { Name = "Ganondorf" };
+            var samurai3 = new Samurai { Name = "Midna" };
+
+            context.Samurais.AddRange(samurai1, samurai2, samurai3);
+            context.SaveChanges();
         }
 
         private static void ClearDatabase() //Fråga: hur tänka när man ska veta vilka man behöver ta bort?
         {
+            //MÅSTE TA HAND OM CONSTRAINT FÖR ATT TA BORT 
             var context = new SamuraiContext();
 
             foreach (var samurai in context.Samurais)
